9b4517701a729cfb38148c743584a0a94bdac751
pervinpashazade <ParvinKHp@code.local>
2019-06-09 22:34:59 +0400

    ok

---
+++ Template3/WebApp3(09.06.2019)/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WebApp3_09._06._2019_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Template3/WebApp3(09.06.2019)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApp3_09._06._2019_.MvcApplication" Language="C#" %>
