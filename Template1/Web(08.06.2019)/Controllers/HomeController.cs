7717833745b6a7a0df9c359930f1bef7a9a41d01
pervinpashazade <ParvinKHp@code.local>
2019-06-09 16:34:39 +0400

    template1

---
+++ Template1/Web(08.06.2019)/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Web_08._06._2019_.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+    }
+}
---
+++ Template1/Web(08.06.2019)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Web_08._06._2019_.MvcApplication" Language="C#" %>
